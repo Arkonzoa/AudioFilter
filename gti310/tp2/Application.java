3291cd6ae751f43d72f0052ad7f1a5435fd07f02
Arkonzoa <exoarkonzoa@gmail.com>
2014-10-15 12:13:20 -0400

    Validation de Wav

--- gti310/tp2/Application.java
+++ gti310/tp2/Application.java
@@ -1,5 +1,10 @@
 package gti310.tp2;
 
+import java.io.FileNotFoundException;
+
+import gti310.tp2.audio.WaveFilter;
+import gti310.tp2.io.FileSource;
+
 public class Application {
 
 	/**
@@ -7,6 +12,23 @@ public class Application {
 	 * @param args This parameter is ignored
 	 */
 	public static void main(String args[]) {
+		if(args.length != 1) {
+			  System.err.println("Invalid command line, exactly one argument required");
+			}
+		else
+		{	
+			try {
+				FileSource input = new FileSource(args[0]);
+				WaveFilter filter = new WaveFilter();
+				filter.process(input);
+				
+				input.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		System.out.println("Audio Resample project!");
 	}
 }
--- gti310/tp2/audio/AudioFilter.java
+++ gti310/tp2/audio/AudioFilter.java
@@ -1,5 +1,7 @@
 package gti310.tp2.audio;
 
+import gti310.tp2.io.FileSource;
+
 /**
  * An AudioFilter object will modify the input data and produce new values.
  * Each AudioFilter will specify its data source and data sink. Sources and
@@ -17,5 +19,5 @@ public interface AudioFilter {
 	 * Filter the input data.
 	 * The function should make sure the input data is valid beforehand.
 	 */
-	void process();
+	void process(FileSource file);
 }
